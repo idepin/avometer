e2e74008f026e0c739cc81816899b08ba094eb4b
Ervin Ahmad Nur Hidayanto <ervin.4happy@gmail.com>
2021-06-19 15:53:35 +0700

    Asset making

---
+++ Assets/Scripts/UIAnimatorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimatorController : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Show()
+    {
+        anim.SetBool("Show", true);
+    }
+
+    public void Hide()
+    {
+        anim.SetBool("Show", false);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject[] ui;
+    int index;
+
+    public void Show()
+    {
+        ui[index].GetComponent<UIAnimatorController>().Show();
+    }
+
+    public void Next()
+    {
+        if(index < ui.Length)
+        {
+            index += 1;
+        }
+        
+    }
+}
